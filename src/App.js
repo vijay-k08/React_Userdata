41b12be32dbdb59b7eb48c1a3e956d7cfb213652
Vijaya Bhaskar K <karrevijayabhaskar93@gmail.com>
2023-10-16 15:11:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data.users));
+  }, []);
+
+  return (
+    <div>
+      <h1 class="title">Dummy data</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>S.No.</th>
+            <th>Profile Pic</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Domain</th>
+            <th>IP</th>
+            <th>University</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>
+                <img src={user.image} alt={user.firstName} width={30} />
+              </td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.gender}</td>
+              <td>{user.email}</td>
+              <td>{user.username}</td>
+              <td>{user.domain}</td>
+              <td>{user.ip}</td>
+              <td>{user.university}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
